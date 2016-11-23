4bc97206e39a6806ad7cd4926ae2dbe5e92670ac
Jair Garcia <ejairn@gmail.com>
2016-11-23 13:30:53 -0500

    Información de los mapas

--- src/assets/js/maps.js
+++ src/assets/js/maps.js
@@ -1,174 +1,71 @@
-function initMap() {
-    let $hospitales = $( '.maps__tabs__list-item:first-of-type' );
-    let $muestras = $( '.maps__tabs__list-item:nth-of-type(2)' );
-    let $medicos = $( '.maps__tabs__list-item:nth-of-type(3)' );
-    let uluru = {lat: -5.363, lng: 131.044};;
+
+    function initMap() {
+
+        let $hospitales = $( '.maps__tabs__list-item:first-of-type' );
+        let $muestras = $( '.maps__tabs__list-item:nth-of-type(2)' );
+        let $medicos = $( '.maps__tabs__list-item:nth-of-type(3)' );
+        let $close = $( ".close" );
+        let $mapsTabsAnchor = $("#maps-tabs li a");
 
 
-    $hospitales.on( 'click', () => {
-        uluru = {lat: -5.363, lng: 131.044};
-        console.log( 'hospitales active' );
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    }); var contentString = '<div id="content">'+
-        '<div id="siteNotice">'+
-        '</div>'+
-        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-        '<div id="bodyContent">'+
-        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-        'sandstone rock formation in the southern part of the '+
-        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-        'south west of the nearest large town, Alice Springs; 450&#160;km '+
-        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-        'Aboriginal people of the area. It has many springs, waterholes, '+
-        'rock caves and ancient paintings. Uluru is listed as a World '+
-        'Heritage Site.</p>'+
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-        '(last visited June 22, 2009).</p>'+
-        '</div>'+
-        '</div>';
+        showMap('Clínica UC','<b>Ubicación</b> Lira 40 Metro U. Católica<br /> Santiago<br /> <br /><b>Teléfono</b><br />22384 6000<br />','-33.44183','-70.64121','hospital','http://redsalud.uc.cl/medios/thumbnail/clinica-lira/home-clinica-lira-288x80.png');
 
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
-    });
 
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-      title: 'Uluru (Ayers Rock)'
-    });
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
+        $hospitales.on( 'click', () => {
+            showMap('Clínica UC','<b>Ubicación</b> Lira 40 Metro U. Católica<br /> Santiago<br /> <br /><b>Teléfono</b><br />22384 6000<br />','-33.44183','-70.64121','hospital','http://redsalud.uc.cl/medios/thumbnail/clinica-lira/home-clinica-lira-288x80.png');
+        });
 
-    });
+        $muestras.on( 'click', () => {
+            showMap('Centro de Especialidades Médicas','<b>Ubicación</b> Marcoleta 350, Metro U. Católica<br /><b>Teléfono</b><br /><b>Informaciones y códigos:</b> 22354 8636','-33.442137','-70.64010','utm','http://redsalud.uc.cl/medios/thumbnail/laboratorios/UTM/utm-cem-288x80.png');
+        });
 
-    $muestras.on( 'click', () => {
-        uluru = {lat: -55.363, lng: 13.044};
-        console.log( 'muestras active' );
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    }); var contentString = '<div id="content">'+
-        '<div id="siteNotice">'+
-        '</div>'+
-        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-        '<div id="bodyContent">'+
-        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-        'sandstone rock formation in the southern part of the '+
-        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-        'south west of the nearest large town, Alice Springs; 450&#160;km '+
-        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-        'Aboriginal people of the area. It has many springs, waterholes, '+
-        'rock caves and ancient paintings. Uluru is listed as a World '+
-        'Heritage Site.</p>'+
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-        '(last visited June 22, 2009).</p>'+
-        '</div>'+
-        '</div>';
+        $medicos.on( 'click', () => {
+            showMap('Centro Médico San Joaquín', '<b>Ubicación</b> Av. Vicuña Mackenna 4686 <br /> Metro San Joaquín<br /> <br /><b>Teléfono</b><br />Reserva horas e informaciones<br />22676 7000  ','-33.497349','-70.61481','centro','http://redsalud.uc.cl/medios/thumbnail/centros_medicos/sanjoaquin-288x80.png');
+        });
 
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
-    });
+        $mapsTabsAnchor.click( function() {
+            var selector = this.id.substring(this.id.indexOf("_") + 1);
+            $("#" + selector).siblings("ul").fadeOut().delay(800).hide();
+            $("#" + selector).fadeIn();
+        });
 
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-      title: 'Uluru (Ayers Rock)'
-    });
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
+        $close.click( function() {
+            $('ul.content').fadeOut();
+        });
 
-    });
-
-    $medicos.on( 'click', () => {
-        uluru = {lat: -80.363, lng: 13.044};
-        console.log( 'medicos active' );
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    }); var contentString = '<div id="content">'+
-        '<div id="siteNotice">'+
-        '</div>'+
-        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-        '<div id="bodyContent">'+
-        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-        'sandstone rock formation in the southern part of the '+
-        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-        'south west of the nearest large town, Alice Springs; 450&#160;km '+
-        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-        'Aboriginal people of the area. It has many springs, waterholes, '+
-        'rock caves and ancient paintings. Uluru is listed as a World '+
-        'Heritage Site.</p>'+
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-        '(last visited June 22, 2009).</p>'+
-        '</div>'+
-        '</div>';
-
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
-    });
-
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-      title: 'Uluru (Ayers Rock)'
-    });
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
-
-    });
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    }); var contentString = '<div id="content">'+
-        '<div id="siteNotice">'+
-        '</div>'+
-        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-        '<div id="bodyContent">'+
-        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-        'sandstone rock formation in the southern part of the '+
-        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-        'south west of the nearest large town, Alice Springs; 450&#160;km '+
-        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-        'Aboriginal people of the area. It has many springs, waterholes, '+
-        'rock caves and ancient paintings. Uluru is listed as a World '+
-        'Heritage Site.</p>'+
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-        '(last visited June 22, 2009).</p>'+
-        '</div>'+
-        '</div>';
-
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
-    });
-
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-      title: 'Uluru (Ayers Rock)'
-    });
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
+    }
 
 
 
+    function showMap( title, ubication, latitude, longitude, type, pic ) {
+        console.log( $( this ) );
 
-}
+        let ubicationPosition = { lat: parseFloat(latitude), lng: parseFloat(longitude) };
+
+        let map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: ubicationPosition,
+          scrollwheel: false
+        });
+
+        let contentString =
+            '<div id="content">'+
+                '<div id="siteNotice"><img src="' + pic + '"></div>'+
+                '<p id="firstHeading" class="firstHeading">'+ title +'</p>'+
+                '<div id="bodyContent">'+
+                    '<p>'+ ubication +'</p>'+
+                '</div>'+
+            '</div>';
+
+        let infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
+        let marker = new google.maps.Marker({
+          position: ubicationPosition,
+          map: map
+        });
+
+        infowindow.open(map, marker);
+
+    }
