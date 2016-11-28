9ab21bbb73a96b102f9ef4d16b24ffb135275d58
Jair Garcia <ejairn@gmail.com>
2016-11-28 14:00:06 -0500

    versión móvil

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,9 +1,27 @@
 
 	let $headerMenu = $( '#headerMenu' );
 	let $headerList = $( '#headerList' );
+	let $headerContainer = $( '#headerContainer' );
+	let $headerLogo = $( '.header__logo' );
 
 	$headerMenu.on( 'click', () => {
 		$headerMenu.toggleClass( 'visible' );
 		$headerList.toggleClass( 'visible' );
-		console.log( 'click' )
+		if( $headerMenu.hasClass( 'visible' ) ) {
+			$headerLogo.removeClass( 'text-center' );
+		}
+		else {
+			$headerLogo.addClass( 'text-center' );
+		}
 	});
+
+
+	$headerContainer.on('sticky.zf.stuckto:top', function() {
+		$headerMenu.addClass( 'visible' );
+		$headerList.addClass( 'visible' );
+		$headerLogo.removeClass( 'text-center' );
+	}).on('sticky.zf.unstuckfrom:top', function(){
+		$headerLogo.addClass( 'text-center' );
+	  $headerMenu.removeClass( 'visible' );
+	  $headerList.removeClass( 'visible' );
+	})
